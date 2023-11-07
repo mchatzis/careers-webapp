fbd11cf939724161d7c120bb0b4580fba34c7085
Michail Chatzis <michatzis@gmail.com>
2023-11-07 12:35:24 +0200

    added frontend query variables

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,25 +2,27 @@ import './main.css';
 import React from 'react';
 import { gql, useQuery } from '@apollo/client';
 
-const GET_COMPANIES = gql`
-    query GetCompanies {
-        companies {
+const GET_JOBS = gql`
+    query QueryJobs($title: String, $location: String) {
+        jobs(title: $title, location: $location) {
             id
-            name
-            pagination_url
-            url
-            jobs {
+            title
+            location
+            dateAdded
+            company {
                 id
-                title
-                location
-                date_added
+                name
             }
         }
     }
 `;
 
 export default function App(){
-    const { loading, error, data } = useQuery(GET_COMPANIES);
+    const title = "cfo";
+    const location = "Madrid";
+    const { loading, error, data } = useQuery(GET_JOBS, {
+        variables: {title, location}
+    });
 
 
     if (loading) return <p>Loading...</p>;
@@ -30,10 +32,10 @@ export default function App(){
         <>
             <p className='font-bold text-gray-500'>Hello World!</p>
             <ul>
-                {data.companies.map(({name, industry}) => {
+                {data.jobs.map(({id, title}) => {
                     return (
-                            <li key={name}>
-                                {name}, {industry}
+                            <li key={id}>
+                                {id}, {title}
                             </li>
                         );
                     })
